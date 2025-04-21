0bca42a1fffe1bdbaa3dccbb619bd1540bef7422
Jacob Kinchen <144300546+Jacon-ator@users.noreply.github.com>
2025-04-21 16:27:00 -0600

    Updated Rules Button Sprite

--- final_project_flutter_app/lib/src/components/rules_button.dart
+++ final_project_flutter_app/lib/src/components/rules_button.dart
@@ -5,24 +5,30 @@ import 'package:final_project_flutter_app/poker_party.dart';
 
 class RulesButton extends PositionComponent
     with TapCallbacks, HasGameRef<PokerParty> {
+  Sprite? sprite;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await Sprite.load("art/buttons/Rules Button.png");
+
+    // Optionally, adjust the size based on your asset's dimensions.
+    if (sprite != null) {
+      size = sprite!.srcSize;
+
+      position = Vector2(
+        gameRef.size.x / 2 - size.x / 2,
+        gameRef.size.y / 2 - size.y / 2,
+      );
+    }
+  }
+
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    final paint = Paint()..color = const Color(0xFF0000FF);
-    canvas.drawRect(size.toRect(), paint);
-
-    final textPainter = TextPainter(
-      text: TextSpan(
-        text: 'Texas Hold\'em Rules',
-        style: TextStyle(color: Colors.white, fontSize: 24),
-      ),
-      textDirection: TextDirection.ltr,
-    );
-    textPainter.layout();
-    textPainter.paint(
-        canvas,
-        Offset(size.x / 2 - textPainter.width / 2,
-            size.y / 2 - textPainter.height / 2));
+    if (sprite != null) {
+      sprite!.renderRect(canvas, size.toRect());
+    }
   }
 
   @override
