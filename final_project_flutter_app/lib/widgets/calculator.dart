ef032cda3cb95b474ec9df10f394f30f4e2812eb
Aidan Eiler <eileran@proton.me>
2025-04-23 02:57:12 -0500

    added additional factory constroctor

--- final_project_flutter_app/lib/widgets/calculator.dart
+++ final_project_flutter_app/lib/widgets/calculator.dart
@@ -54,4 +54,54 @@ class Card {
   /// override hashCode for proper use in collections
   @override
   int get hashCode => rank.hashCode ^ suit.hashCode;
+
+  /// creates a card from a standard string representation (e.g., "AS" for Ace of Spades)
+  factory Card.fromString(String str) {
+    if (str.length != 2)
+      throw FormatException('Card string must be 2 characters');
+
+    Rank rank;
+    switch (str[0]) {
+      case 'A':
+        rank = Rank.ace;
+        break;
+      case 'K':
+        rank = Rank.king;
+        break;
+      case 'Q':
+        rank = Rank.queen;
+        break;
+      case 'J':
+        rank = Rank.jack;
+        break;
+      case 'T':
+        rank = Rank.ten;
+        break;
+      default:
+        final rankValue = int.tryParse(str[0]);
+        if (rankValue == null || rankValue < 2 || rankValue > 9)
+          throw FormatException('Invalid rank: ${str[0]}');
+        rank = Rank.values[rankValue - 2];
+    }
+
+    Suit suit;
+    switch (str[1]) {
+      case 'C':
+        suit = Suit.clubs;
+        break;
+      case 'D':
+        suit = Suit.diamonds;
+        break;
+      case 'H':
+        suit = Suit.hearts;
+        break;
+      case 'S':
+        suit = Suit.spades;
+        break;
+      default:
+        throw FormatException('Invalid suit: ${str[1]}');
+    }
+
+    return Card(rank, suit);
+  }
 }
