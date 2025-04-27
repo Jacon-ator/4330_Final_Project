1ad5c57d75b3639067719baba3476a33f7649d19
dor8d0 <troyw792@gmail.com>
2025-04-27 00:58:22 -0500

    changes the icon of the login button

---
+++ final_project_flutter_app/lib/src/components/login_button.dart
@@ -0,0 +1,34 @@
+import 'package:final_project_flutter_app/widgets/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final String name;
+
+  const LoginButton({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SignUpPage(name: name),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white, // white circular background
+        ),
+        child: const Icon(
+          Icons.login,
+          size: 32, // size of the icon
+          color: Colors.black, // icon color
+        ),
+      ),
+    );
+  }
+}
