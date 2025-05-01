8669b2d622b9e6e1de13b6436e701538baeb87b1
AustinCao <austincao23@gmail.com>
2025-04-30 23:36:17 -0500

    Update buy_table_skin_button.dart

--- final_project_flutter_app/lib/components/buttons/shop/buy_table_skin_button.dart
+++ final_project_flutter_app/lib/components/buttons/shop/buy_table_skin_button.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project_flutter_app/poker_party.dart';
 import 'package:final_project_flutter_app/screens/shop_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
@@ -28,8 +30,23 @@ class BuyTableSkinButton extends PositionComponent with TapCallbacks, HasGameRef
   }
 
   @override
-  void onTapDown(TapDownEvent event) {
-    ShopScreen.ownsTableSkin = true;
-    print("Table Skin bought!");
+  void onTapDown(TapDownEvent event) async {
+    if (ShopScreen.ownsTableSkin) {
+      print("Table Skin already owned.");
+      return;
+    }
+
+    if (ShopScreen.coinBalance >= 1000) {
+      ShopScreen.coinBalance -= 1000;
+      ShopScreen.ownsTableSkin = true;
+      print("Table Skin bought! Remaining coins: ${ShopScreen.coinBalance}");
+
+      final email = FirebaseAuth.instance.currentUser?.email;
+      if (email != null) {
+        await FirebaseFirestore.instance.collection("users").doc(email).update({"Money": ShopScreen.coinBalance,});
+      }
+    } else {
+      print("Not enough coins to buy Table Skin. You have ${ShopScreen.coinBalance}.");
+    }
   }
 }
